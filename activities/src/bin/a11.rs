3f32314ed53b5844afdceab4b7452b5587223a3d
yussaaa <yusa.li@mail.utoronto.ca>
2024-01-03 07:58:21 -0500

    Memory model, move and model

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -9,4 +9,23 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct GroceryItem {
+    quantity: i32,
+    id: i32,
+}
+
+fn display_quantity(item:&GroceryItem) {
+    println!("Quantity: {}", item.quantity);
+}
+fn display_id(item: &GroceryItem) {
+    println!("ID: {}", item.id);
+}
+fn main() {
+    let item = GroceryItem {
+        quantity: 5,
+        id: 100,
+    };
+
+    display_quantity(&item);
+    display_id(&item);
+}
